6abe015d10b50937bef23348aac7db32095b1000
arunprksm <97307361+arunprksm@users.noreply.github.com>
2023-04-12 03:30:06 +0530

    added new input system package... GameInput script and modified... added player input actions and generated scripts...
    updated player move input actions with wasd, arrow and gamepad...

--- KitchenChaos/Assets/_Project/_Scripts/Player.cs
+++ KitchenChaos/Assets/_Project/_Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -10,29 +11,12 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float playerMoveSpeed = 7.0f;
     [SerializeField] private float playerRotationRate = 10.0f;
+    [SerializeField] private GameInput gameInput = null;
 
     private bool isWalking = false;
     private void Update()
     {
-
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, transform.position.y, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         transform.position += playerMoveSpeed * Time.deltaTime * moveDir;
