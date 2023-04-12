c49428b23ab4b9cc16545babe8d7b764cc8054fe
arunprksm <97307361+arunprksm@users.noreply.github.com>
2023-04-12 21:17:48 +0530

    collision is detected using player can move bool value...

--- KitchenChaos/Assets/_Project/_Scripts/Player.cs
+++ KitchenChaos/Assets/_Project/_Scripts/Player.cs
@@ -18,9 +18,45 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, transform.position.y, inputVector.y);
+        float playerRadius = 0.7f;
+        float playerHeight = 2.0f;
+        float moveDistance = playerMoveSpeed * Time.deltaTime;
+        bool playerCanMove = !Physics.CapsuleCast(transform.position, transform.position + transform.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!playerCanMove)
+        {
+            //Cannot move towards moveDir
+
+            //Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            playerCanMove = !Physics.CapsuleCast(transform.position, transform.position + transform.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (playerCanMove)
+            {
+                //playerCanMove only on X direction
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //Cannot move towards moveDir
+
+                //Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                playerCanMove = !Physics.CapsuleCast(transform.position, transform.position + transform.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (playerCanMove)
+                {
+                    //playerCanMove only on Z direction
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot move in any Direction
+                }
+            }
+        }
+        if (playerCanMove) transform.position += moveDistance * moveDir;
         isWalking = moveDir != Vector3.zero;
-        transform.position += playerMoveSpeed * Time.deltaTime * moveDir;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, playerRotationRate * Time.deltaTime);
+        //Debug.Log(moveDir);
     }
 
     public bool IsWalking()
